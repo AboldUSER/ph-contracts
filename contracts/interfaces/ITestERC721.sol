97469d689bf333092a374d571e2188f4350fad89
bold <2985057+AboldUSER@users.noreply.github.com>
2022-03-10 00:03:02 -0800

    Clean up interface

--- contracts/interfaces/ITestERC721.sol
+++ contracts/interfaces/ITestERC721.sol
@@ -3,20 +3,8 @@
 pragma solidity ^0.8.0;
 
 interface ITestERC721 {
-    //Types
-    // 1 staking
-    // 2 offset
-    // 3 steal
-    // 4 protect high
-    // 5 protect mid
-    // 6 protect low
-    // function mint() external;
-    // function balanceOf(address) external view returns (uint256);
-    // function tokenBalance(address) external view returns (uint256);
 
     function balanceOf(address) external view returns (uint256);
-
-    function getNFTOwners(uint256) external view returns (address[] memory);
 
     function batchTransferFrom(
         address _from,
@@ -45,9 +33,4 @@ interface ITestERC721 {
         uint256 tokenId,
         bytes memory data
     ) external;
-
-    // function getNFTType(uint256) external view returns(address, bytes32);
-    // function tokenType(uint256) external view returns(bytes32);
-    // function getAlfaOwners() external view returns(address[] memory);
-    // function tokenGenerationTimestamp(uint256) external view returns(uint256);
 }
